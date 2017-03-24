89d1e24840b3c1293150597054e64c1426f0d388
Rhys Brame <rhysbrame@gmail.com>
2017-03-24 13:04:23 +0000

    added funds to the customer

--- models/customer.rb
+++ models/customer.rb
@@ -5,10 +5,11 @@ class Customer
   def initialize( options )
     @id = options['id'].to_i
     @name = options['name']
+    @funds = options['funds'].to_i
   end
 
   def save()
-    sql = "INSERT INTO customers (name) VALUES ('#{@name}') RETURNING id"
+    sql = "INSERT INTO customers (name, funds) VALUES ('#{@name}', #{@funds}) RETURNING id"
     customer = SqlRunner.run(sql).first
     @id = customer['id'].to_i
   end
